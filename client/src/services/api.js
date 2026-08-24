2a892f168b5cb5b33ea78128c4727bdafe58a80e
Ansh Gupta <ng607301@gmail.com>
2026-08-24 18:03:36 +0530

    Resolve merge conflict

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,11 +4,8 @@ import axios from 'axios';
 const baseURL = import.meta.env.VITE_API_URL || 'https://radhakripamandal.onrender.com/api';
 
 const API = axios.create({
-<<<<<<< HEAD
   baseURL: 'https://radhakripamandal.onrender.com/api',
-=======
   baseURL,
->>>>>>> 432a08c665927c6ebd916398402dc804767d156f
   headers: {
     'Content-Type': 'application/json',
   },
